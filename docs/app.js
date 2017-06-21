c3dd7e991c600dd1bd482e0285649cb4c0369d24
tekkyuu <byabyabya.dx@gmail.com>
2017-06-21 19:31:55 +0900

    add alert

--- docs/app.js
+++ docs/app.js
@@ -1,19 +1,18 @@
 window.onload = function(){
-    console.log("deeplinking");
     var facebooklinkList = document.querySelectorAll('.fblink');
 
+    alert('start linking');
     facebooklinkList.forEach(function(facebooklink) {
-        console.log(facebooklink);
+        alert('linking now');
 
         //通常リンクを取得
         var normalLink = facebooklink.getAttribute('href');
-        console.log(normalLink);
+
         //通常リンクを無効にする
         facebooklink.setAttribute('href','javascript:void(0)');
 
         //ディープリンク用リンクを取得
         var deeplink = facebooklink.getAttribute('fbhref');
-        console.log(deeplink);
 
 
         facebooklink.addEventListener('click',function(e){
